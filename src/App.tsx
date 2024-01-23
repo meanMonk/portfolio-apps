b1426685884651e604c6b2ee53e617b87cb967f2
sahil kashetwar <sahilkashetwar24@gmail.com>
2024-01-23 21:57:06 +0530

    todowith useContext & useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import FeedbackForm from './examples/react-hook-form/ContactForm'
 import TodoListContainer from './examples/todoList/todoListContainer'
+import TodoListWithContext from './examples/todoList/todoListWithContext'
 
 function App() {
   return (
     <main className="container-fluid flex items-center flex-col gap-3 justify-center my-20 w-full">
+      <TodoListWithContext />
       <TodoListContainer />
       <FeedbackForm />
     </main >
